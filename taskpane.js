8749f737f02c050717d85d732961043092c66d47
Bùi Quang Trung <buiquangtrung2012@gmail.com>
2026-04-17 13:20:01 +0700

    v17042026.1240 - Cập nhật từ local

--- taskpane.js
+++ taskpane.js
@@ -1,6 +1,6 @@
-import { WordService } from './word_service.js?v=17042026.1230';
-import { StorageService } from './storage_service.js?v=17042026.1230';
-import { MockData } from './mock_data.js?v=17042026.1230';
+import { WordService } from './word_service.js?v=17042026.1240';
+import { StorageService } from './storage_service.js?v=17042026.1240';
+import { MockData } from './mock_data.js?v=17042026.1240';
 
 /* global Office, lucide */
 
