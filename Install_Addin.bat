6a41cb6eacac0f5b55f27c4e258a224e920f117b
Bùi Quang Trung <buiquangtrung2012@gmail.com>
2026-04-24 17:16:22 +0700

    v24042026.1659 - Cập nhật từ local

--- Install_Addin.bat
+++ Install_Addin.bat
@@ -4,37 +4,37 @@ chcp 65001 >nul
 
 set "INSTALL_DIR=%APPDATA%\HoSoAddin"
 set "GITHUB_URL=https://raw.githubusercontent.com/buiquangtrung2012-ops/HoSoAddin/main"
-set "CATALOG_GUID={B1A9908E-1C4F-40E2-9EED-7C919D12DF01}"
 
 echo ==================================================
-echo CHUONG TRINH CAI DAT ADD-IN (TU DONG HOAN TOAN)
+echo CHUONG TRINH CAI DAT ADD-IN (FIX TRUNCATION)
 echo ==================================================
 
 :: 1. Tao thu muc
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 
-echo [1/5] Dang tai file manifest.xml moi nhat...
+echo [1/4] Dang tai file manifest.xml moi nhat...
 powershell -ExecutionPolicy Bypass -Command "(New-Object Net.WebClient).DownloadFile('%GITHUB_URL%/manifest.xml', '%INSTALL_DIR%\manifest.xml')"
 
-echo [2/5] Dang tai file Mau_ToTrinh.docx...
+echo [2/4] Dang tai file Mau_ToTrinh.docx...
 powershell -ExecutionPolicy Bypass -Command "(New-Object Net.WebClient).DownloadFile('%GITHUB_URL%/Mau_ToTrinh.docx', '%INSTALL_DIR%\Mau_ToTrinh.docx')"
 
-echo [3/5] Don dep cache va cac thiet lap cu...
+echo [3/4] Xoa bo nho dem va cac thiet lap cu...
 rmdir /s /q "%LocalAppData%\Microsoft\Office\16.0\Wef" >nul 2>&1
-REG DELETE "HKCU\Software\Microsoft\Office\16.0\WEF\Developer" /v "HoSoAddin_GitHub" /f >nul 2>&1
-REG DELETE "HKCU\Software\Microsoft\Office\16.0\WEF\Developer" /v "HoSoAddinDev" /f >nul 2>&1
-REG DELETE "HKCU\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\%CATALOG_GUID%" /f >nul 2>&1
 
-echo [4/5] Dang ky Trusted Catalog (Add-in Menu)...
-REG ADD "HKCU\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\%CATALOG_GUID%" /v "Id" /t REG_SZ /d "%CATALOG_GUID%" /f >nul
-REG ADD "HKCU\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\%CATALOG_GUID%" /v "Url" /t REG_SZ /d "%INSTALL_DIR%" /f >nul
-REG ADD "HKCU\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\%CATALOG_GUID%" /v "Flags" /t REG_DWORD /d 1 /f >nul
-
-echo [5/5] Dang ky Trusted Location (He thong tin cay)...
-set "TRUSTED_LOC_KEY=HKCU\Software\Microsoft\Office\16.0\Word\Security\Trusted Locations\HoSoAddin"
-REG ADD "%TRUSTED_LOC_KEY%" /v "Path" /t REG_SZ /d "%INSTALL_DIR%" /f >nul
-REG ADD "%TRUSTED_LOC_KEY%" /v "AllowSubfolders" /t REG_DWORD /d 1 /f >nul
-REG ADD "%TRUSTED_LOC_KEY%" /v "Description" /t REG_SZ /d "HoSoAddin Catalog" /f >nul
+echo [4/4] Dang ky he thong tin cay bang PowerShell (Cuc ky chinh xac)...
+powershell -ExecutionPolicy Bypass -Command ^
+    "$dir = '%INSTALL_DIR%';" ^
+    "$guid = '{B1A9908E-1C4F-40E2-9EED-7C919D12DF01}';" ^
+    "$wefPath = \"HKCU:\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\$guid\";" ^
+    "if (!(Test-Path $wefPath)) { New-Item -Path $wefPath -Force };" ^
+    "Set-ItemProperty -Path $wefPath -Name 'Id' -Value $guid;" ^
+    "Set-ItemProperty -Path $wefPath -Name 'Url' -Value $dir;" ^
+    "Set-ItemProperty -Path $wefPath -Name 'Flags' -Value 1 -PropertyType DWord;" ^
+    "$wordLoc = 'HKCU:\Software\Microsoft\Office\16.0\Word\Security\Trusted Locations\HoSoAddin';" ^
+    "if (!(Test-Path $wordLoc)) { New-Item -Path $wordLoc -Force };" ^
+    "Set-ItemProperty -Path $wordLoc -Name 'Path' -Value $dir;" ^
+    "Set-ItemProperty -Path $wordLoc -Name 'AllowSubfolders' -Value 1 -PropertyType DWord;" ^
+    "Set-ItemProperty -Path $wordLoc -Name 'Description' -Value 'HoSoAddin Catalog';"
 
 echo.
 echo ==================================================
@@ -45,6 +45,6 @@ echo 1. Tat tat ca cac cua so Word dang mo.
 echo 2. Mo lai Word.
 echo 3. Vao tab Insert -^> My Add-ins.
 echo 4. Bam nut "Refresh" (lam moi) o goc tren ben phai.
-echo 5. Tab "SHARED FOLDER" se xuat hien va co Add-in o do.
+echo 5. Tab "SHARED FOLDER" se hien ra va co Add-in o do.
 echo ==================================================
 pause
